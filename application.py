d04e4b1550be09e5de895dad27149630d120515f
Cecilia Olivera <ceci.coding@gmail.com>
2020-10-23 21:14:33 +0200

    finished hero and search section

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ TEMPLATES_AUTO_RELOAD=True
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Find Events")
     
 if __name__ == "__main__":
     app.run(debug=True)
