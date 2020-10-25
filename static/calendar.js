1aa65560b146fcadf0681b55f79ceb889d269535
Cecilia Olivera <ceci.coding@gmail.com>
2020-10-26 00:10:31 +0100

    almost finished calendar

---
+++ static/calendar.js
@@ -0,0 +1,63 @@
+//TODO: Next days not rendering well
+
+'use strict';
+
+const date = new Date();
+
+const renderCalendar = () => {
+    const monthDays = document.querySelector('.days');
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    const firstDayIndex = date.getDay();
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+    const nextDays = 7 - lastDayIndex - 1;
+
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    document.querySelector('.date h3').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
+
+    let days = '';
+
+    for (let i = firstDayIndex; i > 0; i--) {
+        days += `<div class="prev-date">${prevLastDay - i + 1}</div>`;
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+        days += `<div>${i}</div>`;
+        monthDays.innerHTML = days;
+    }
+
+    for (let i = 1; i <= nextDays; i++) {
+        days += `<div class="next-date">${i}</div>`;
+        monthDays.innerHTML = days;
+    }
+
+}
+
+
+
+//events listeners for arrows
+document.querySelector('.prev-month-arrow').addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+});
+
+document.querySelector('.next-month-arrow').addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+});
+
+renderCalendar();
