34ea3308593bddd2e00f35af11f6cf1bdf00f419
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-06 18:25:58 +0100

    solved bug in events menu

--- app/static/events.js
+++ app/static/events.js
@@ -6,3 +6,12 @@ titles.forEach(title =>  title.innerHTML = title.innerHTML.replace(/^(.{14}[^\s]
 //display dates correctly
 const dates = Array.from(document.querySelectorAll('.event-date'));
 dates.forEach(date => date.innerHTML = new Date(date.innerHTML).toString().slice(0, 16));
+
+//underline menu-items links
+const eventsMenu = document.querySelector('.events-menu');
+
+eventsMenu.addEventListener('click', (e) => {
+    const eventFilters = document.querySelectorAll('.events-menu-item');
+    eventFilters.forEach(filter => filter.classList.remove('active'))
+    e.target.classList.add('active');
+})
