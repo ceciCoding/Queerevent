6cfa60b66df62dbc7fb494f40591d2e362f2fb7d
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-07 15:00:08 +0100

    dynamic width for mobile nav

--- app/static/layout.js
+++ app/static/layout.js
@@ -1,7 +1,8 @@
 'use strict'
-const menu = document.querySelector('.nav-container ul') || document.querySelector('.mobile-user')
+const desktopMenu = document.querySelector('.nav-container ul');
+const mobileMenu = document.querySelector('.mobile-user');
 
-menu.addEventListener('click', (e) => {
+desktopMenu.addEventListener('click', (e) => {
     const userImage = document.querySelector('#user-img')
     const logoutMenu = document.querySelector('.logout');
     console.log(e.target)
@@ -10,3 +11,8 @@ menu.addEventListener('click', (e) => {
         console.log(logoutMenu);
     }
 })
+
+//dynamic width for mobile nav
+const menuContainer = document.querySelector('.mobile-nav ul');
+const main = document.querySelector('main');
+menuContainer.style.width = `${main.offsetWidth * 1.2}px`;
