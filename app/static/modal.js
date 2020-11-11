f81761cb2be3e71da6cdfba5c10068940ce9f857
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-11 12:19:53 +0100

    solved bug in copy modal link logic

--- app/static/modal.js
+++ app/static/modal.js
@@ -13,12 +13,22 @@ trigger.addEventListener('click', (e) => {
     if ((e.target.type === 'text' || e.target.type === 'select') && e.target.id != 'search') {
         e.preventDefault();
     }
+
+    //modal 
     if (e.target.className.includes('share-arrow') || e.target.className.includes('open-m')) {
         if (modalLink.style.display != 'none') {
             modalLink.value = modalLink.value.concat(e.target.getAttribute("data-event-id"))
         }
         openModal(modal, overlay);
     }
+
+     //copy link text in share modal
+    if (e.target.id === 'copy') {
+        modalLink.select();
+        modalLink.setSelectionRange(0, 99999);
+        document.execCommand('copy');
+    }
+
     if (e.target.className.includes('close-button')) {
         closeModal(modal, overlay);
         if (modalLink.style.display != 'none') {
@@ -26,11 +36,42 @@ trigger.addEventListener('click', (e) => {
         }
     }
 
-    //copy link text in share modal
-    if (e.target.id === 'copy') {
-        modalLink.select();
-        modalLink.setSelectionRange(0, 99999);
-        document.execCommand('copy');
+    //favorites 
+    if (e.target.className.includes('heart')) {
+        console.log(e.target)
+        if (e.target.className.includes('active')) {
+            e.target.classList.remove('active');
+            e.target.setAttribute("name", "heart-outline");
+        } else {
+            e.target.classList.add('active');
+            e.target.setAttribute("name", "heart")
+        }
+        const eventData = {
+            event: e.target.getAttribute(['data-event'])
+        }
+
+        fetch(`${window.origin}/toggle-favorite`, {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(eventData),
+            cache: "no-cache",
+            headers: new Headers({
+                'content-type': 'application/json'
+            })
+        })
+            .then(response => {
+                if (response.status !== 200) {
+                    console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+                    return
+                }
+                response.json().then(data => {
+                    console.log(data);
+                });
+            })
+            .catch(error => {
+                console.log(`Fetch error ${error}`)
+            })
+        
     }
 
 });
