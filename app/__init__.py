f5cbd642e79024a38e93c339d4b56fcf915eceb3
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-11 17:38:20 +0100

    trying to deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 import os
 import logging.handlers
 from logging.handlers import RotatingFileHandler
-from config import Config
+# from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
+# app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager()
